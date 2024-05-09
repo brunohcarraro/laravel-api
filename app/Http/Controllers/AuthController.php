7a7430c064cc9a4f4582393d59a0a730c8cf90af
brunohcarraro <brunocrro@hotmail.com>
2024-05-09 06:26:45 -0300

    Push files

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+    	$fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ], [
+            'name.required' => 'Por favor, preencha seu nome.',
+            'email.required' => 'Por favor, preencha seu email.',
+            'email.email' => 'Preencha um e-mail válido',
+            'password.required' => 'Por favor, digite uma senha',
+            'password.min' => 'A senha deve conter no mínimo :min caracteres',
+            'password.confirmed' => 'As senhas não conferem'
+        ]);
+       
+    	$user = User::create([
+    		'name' => $fields['name'],
+    		'email' => $fields['email'],
+    		'password' => bcrypt($fields['password'])
+    	]);
+
+    	$token = $user->createToken('myapptoken')->plainTextToken;
+
+    	$response = [
+    		'user' => $user,
+    		'token' => $token
+    	];
+
+    	return response($response, 201);
+
+    }
+
+    public function login(Request $request)
+    {
+    	$fields = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ], [
+            'email.required' => 'Por favor, preencha seu email.',
+            'email.email' => 'Preencha um e-mail válido',
+            'password.required' => 'Por favor, digite sua senha',
+        ]);
+       
+    	$user = User::where('email', $fields['email'])->first();
+
+    	if (!$user || !Hash::check($fields['password'], $user->password)) {
+    		return response([
+    			'message' => 'Usuário ou senha incorretos.'
+    		], 401);
+    	}
+
+
+    	$token = $user->createToken('myapptoken')->plainTextToken;
+
+    	$response = [
+    		'user' => $user,
+    		'token' => $token
+    	];
+
+    	return response($response, 201);
+
+    }
+
+    public function logout(Request $request){
+    	auth()->user()->tokens()->delete();
+
+    	return [
+    		'message' => "Usuário deslogado."
+    	];
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+// Rotas públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rotas protegidas
+Route::group(['middleware' => ['auth:sanctum']], function () {
+	Route::resource('products', ProductsController::class);
+	Route::get('/products/search/{nome}', [ProductsController::class, 'search']);
+	Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
